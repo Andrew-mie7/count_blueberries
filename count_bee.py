da3c7e8edc6981b07478b9e59b524e8b89be96a1
Andrew-mie7 <63439849+Andrew-mie7@users.noreply.github.com>
2021-09-29 08:54:16 +0800

    Add  count_bee_result.png

--- count_bee.py
+++ count_bee.py
@@ -21,19 +21,14 @@ def process(im: np.ndarray):
     # cv2.imshow('gray', gray)
 
     blurred = cv2.medianBlur(gray, 9*SCALAR)
-    # cv2.imshow('blurred', blurred)
-    light = cv2.blur(gray, (99*SCALAR, 99*SCALAR))
-    light = cv2.bitwise_not(light)
-    
-    light = cv2.normalize(light, None, 0, 1, cv2.NORM_MINMAX)
-    cv2.imshow('light', light)
+    cv2.imshow('blurred', blurred)
 
     th, bw = cv2.threshold(blurred, TH, 255, cv2.THRESH_BINARY)
     # cv2.imshow('bw', bw)
 
     kernel_erosion = np.ones((ERODE, ERODE), np.uint8)
     eroded = cv2.erode(bw, kernel_erosion, iterations=1)
-    cv2.imshow('eroded', eroded)
+    # cv2.imshow('eroded', eroded)
 
     dist = cv2.distanceTransform(eroded, cv2.DIST_L2, cv2.DIST_MASK_PRECISE)
     dist = cv2.normalize(dist, None, 0, 1, cv2.NORM_MINMAX)
@@ -59,10 +54,10 @@ def process(im: np.ndarray):
     # cv2.imshow('distTempl', distTempl)
 
     matched = cv2.matchTemplate(distborder, distTempl, cv2.TM_CCOEFF_NORMED)
-    cv2.imshow('matched', matched)
+    # cv2.imshow('matched', matched)
     kernel_erosion = np.ones((ERODE, ERODE), np.uint8)
     matched = cv2.erode(matched, kernel_erosion, iterations=1)
-    cv2.imshow('matched2', matched)
+    # cv2.imshow('matched2', matched)
 
     mn, mx, _, _ = cv2.minMaxLoc(matched)
     THRESH = mx*0.25
@@ -92,7 +87,7 @@ def process(im: np.ndarray):
 
 
 # Reading Videos
-capture = cv2.VideoCapture('./video.mp4')
+capture = cv2.VideoCapture('./bee_video.mp4')
 while True:
     isTrue, frame = capture.read()
     if isTrue == True:
